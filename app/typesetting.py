32a9e6159b90e9722bf433f82d69a98453ba18f7
Roberto Arista <arista.rob@gmail.com>
2024-07-25 16:48:27 +0200

    snake case

--- app/typesetting.py
+++ app/typesetting.py
@@ -10,10 +10,10 @@ from ufo2svg.svgPathPen import SVGPathPen
 
 from app.config import settings
 
-fontPath = settings.STATIC_DIR / "fonts/IntelOneMono-Regular.otf"
+FONT_PATH = settings.STATIC_DIR / "fonts/IntelOneMono-Regular.otf"
 
 
-def defaultFeatureToState() -> dict[str, bool]:
+def default_feature_to_state() -> dict[str, bool]:
     return {"kern": True, "locl": True, "liga": True, "calt": True}
 
 
@@ -25,48 +25,48 @@ UNITS_PER_EM_DEFAULT = 1000
 class GlyphRecord:
 
     glyph: Glyf
-    xPlacement: int
-    yPlacement: int
-    xAdvance: int
-    yAdvance: int
+    x_placement: int
+    y_placement: int
+    x_advance: int
+    y_advance: int
 
 
 @dataclass
 class Shaper:
 
-    binaryPath: Path
-    featureToState: dict[str, bool] = field(default_factory=defaultFeatureToState)
-    glyphSet: dict = field(default_factory=dict)
+    binary_path: Path
+    feature_to_state: dict[str, bool] = field(default_factory=default_feature_to_state)
+    glyph_set: dict = field(default_factory=dict)
 
     def __post_init__(self):
-        self.ttFont = TTFont(self.binaryPath)
-        self.glyphSet = cast(dict, self.ttFont.getGlyphSet())
+        self.tt_font = TTFont(self.binary_path)
+        self.glyph_set = cast(dict, self.tt_font.getGlyphSet())
         data = io.BytesIO()
-        self.ttFont.save(data)
+        self.tt_font.save(data)
         face = hb.Face(data.getvalue())  # type: ignore
-        self.hbFont = hb.Font(face)  # type: ignore
+        self.hb_font = hb.Font(face)  # type: ignore
 
     @property
-    def unitsPerEm(self) -> int:
-        return self.ttFont["head"].unitsPerEm  # type: ignore
+    def units_per_em(self) -> int:
+        return self.tt_font["head"].unitsPerEm  # type: ignore
 
     @property
     def ascender(self) -> int:
-        return self.ttFont["OS/2"].sTypoAscender  # type: ignore
+        return self.tt_font["OS/2"].sTypoAscender  # type: ignore
 
     @property
     def descender(self) -> int:
-        return self.ttFont["OS/2"].sTypoDescender  # type: ignore
+        return self.tt_font["OS/2"].sTypoDescender  # type: ignore
 
-    def resetFeatures(self):
-        self.featureToState = defaultFeatureToState()
+    def reset_features(self):
+        self.feature_to_state = default_feature_to_state()
 
     def process(
         self,
         text: str,
         script: str | None = None,
-        langSys: str | None = None,
-        rightToLeft: bool = False,
+        lang_sys: str | None = None,
+        right_to_left: bool = False,
     ) -> list[GlyphRecord]:
         buf = hb.Buffer()  # type: ignore
         buf.add_str(text)
@@ -76,42 +76,42 @@ class Shaper:
 
         if script:
             buf.set_script_from_ot_tag(script)
-        if langSys:
-            buf.set_language_from_ot_tag(langSys)
-        buf.direction = "rtl" if rightToLeft else "ltr"
+        if lang_sys:
+            buf.set_language_from_ot_tag(lang_sys)
+        buf.direction = "rtl" if right_to_left else "ltr"
 
-        hb.shape(self.hbFont, buf, self.featureToState)  # type: ignore
+        hb.shape(self.hb_font, buf, self.feature_to_state)  # type: ignore
 
         infos = buf.glyph_infos
         positions = buf.glyph_positions
 
-        glyphRecords = []
+        glyph_records = []
         for info, pos in zip(infos, positions):
             index = info.codepoint
-            glyphName = self.ttFont.getGlyphName(index)
-            glyphRecords.append(
+            glyph_name = self.tt_font.getGlyphName(index)
+            glyph_records.append(
                 GlyphRecord(
-                    glyph=self.glyphSet[glyphName],
-                    xPlacement=pos.x_offset,
-                    yPlacement=pos.y_offset,
-                    xAdvance=pos.x_advance,
-                    yAdvance=pos.y_advance,
+                    glyph=self.glyph_set[glyph_name],
+                    x_placement=pos.x_offset,
+                    y_placement=pos.y_offset,
+                    x_advance=pos.x_advance,
+                    y_advance=pos.y_advance,
                 )
             )
-        return glyphRecords
+        return glyph_records
 
-    def convertToSVG(self, glyphRecords: list[GlyphRecord]) -> str:
-        if not glyphRecords:
+    def convert_to_svg(self, glyph_records: list[GlyphRecord]) -> str:
+        if not glyph_records:
             return ""
 
         paths = []
-        xRunAdvance = 0
-        yRunAdvance = 0
+        x_run_advance = 0
+        y_run_advance = 0
 
-        for glyphRecord in glyphRecords:
+        for glyph_record in glyph_records:
 
-            svgPen = SVGPathPen(self.glyphSet)
-            glyphRecord.glyph.draw(svgPen)  # type: ignore
+            svgPen = SVGPathPen(self.glyph_set)
+            glyph_record.glyph.draw(svgPen)  # type: ignore
 
             # translate()
             paths.append(
@@ -119,32 +119,31 @@ class Shaper:
                     transform="
                         scale(1 -1)
                         scale(1 1)
-                        translate({xRunAdvance} {-self.ascender-self.descender})"
+                        translate({x_run_advance} {-self.ascender-self.descender})"
                     d="{svgPen.getCommands()}"
                     fill="black"/>
                 """
             )
-            xRunAdvance += glyphRecord.xAdvance
-            yRunAdvance += glyphRecord.yAdvance
+            x_run_advance += glyph_record.x_advance
+            y_run_advance += glyph_record.y_advance
 
-        # width="{xRunAdvance}" height="{self.unitsPerEm}"
         return f"""\
-        <svg viewBox="0 0 {xRunAdvance} {self.unitsPerEm}">
+        <svg viewBox="0 0 {x_run_advance} {self.units_per_em}">
             {''.join(paths)}
         </svg>
         """
 
 
 def typeset(word: str) -> str:
-    shaper = Shaper(fontPath)
+    shaper = Shaper(FONT_PATH)
     records = shaper.process(word)
-    return shaper.convertToSVG(records)
+    return shaper.convert_to_svg(records)
 
 
 if __name__ == "__main__":
     from icecream import ic
 
-    shaper = Shaper(fontPath)
+    shaper = Shaper(FONT_PATH)
     records = shaper.process("ı́")
     ic([i.glyph.name for i in records])  # type: ignore
     ic(records)
